c94f379c038331bc98755ddd7b43a8827a779b0c
Aditya Devarakonda <{ID}+{username}@users.noreply.github.com>
2021-01-04 00:01:22 -0500

    moved from src structure to package structure

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup, find_packages
 
 setup(name="Minerva",
-        packages=find_packages(where='src'),
-        package_dir={"":"src"},
+        packages=find_packages(),
         package_data={"":["*.json"]})
