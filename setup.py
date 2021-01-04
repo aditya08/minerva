6aeda55db897336deda42fc47001a2b4206c6077
Aditya Devarakonda <{ID}+{username}@users.noreply.github.com>
2021-01-03 23:53:58 -0500

    specified packages to be minerva

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 
 setup(name="Minerva",
         package_dir={"minerva":"src"},
-        packages=["minerva"]
+        packages=["minerva"],
         package_data={"":["*.json"]})
